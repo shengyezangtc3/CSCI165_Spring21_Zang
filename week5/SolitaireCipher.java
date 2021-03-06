a4a4b6cc45277f20f9987dad8ede5fd2f38404ec
Shengye Zang <sz247@cornell.edu>
2021-03-05 23:50:41 -0500

    Submission for week 5 lab

--- week5/SolitaireCipher.java
+++ week5/SolitaireCipher.java
@@ -1,10 +1,37 @@
+/**
+ * Author: Shengye Zang
+ * Date: March 5 2020
+ * Assignment: Week 5 Lab
+ */
+
+import java.util.Random;
+import java.util.Arrays;
+import java.lang.Math;
+
 public class SolitaireCipher{
 
     public static void main(String[] args) {
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";             // plain text alphabet
         int[]  cardDeck = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16, // "card deck". Joker A = 27, Joker B = 28
                             17,18,19,20,21,22,23,24,25,26,27,28};
+        
+        // need to repeart steps 1-5 if step 5 returns 0
+        String original_message = args[0];
+        String choice = args[1];
+        int text_length = original_message.length();
+        
+        String prepared_message = prepareMessage(original_message);
+        int[] key_stream = generateKeystream(cardDeck, text_length);
 
+        if(choice.equals("e")) {
+            encode(prepared_message, key_stream);
+        }
+        else if(choice.equals("d")) {
+            decode(prepared_message, key_stream);
+        }
+        else 
+            System.out.println("Invalid choice.");
+        
     }
     
     /**
@@ -39,28 +66,160 @@ public class SolitaireCipher{
     }
 
     // TO DO: Finish these methods
-
-    public static void shuffleDeck(int[] deck){}            // randomize the deck yourself. No shortcuts
-    public static void tripleCut(int[] deck){}              // implements step 3
-    public static void sliceFront(int[] deck){}             // implements step 4
-    public static int  generateKeystreamValue(int[] deck){  // implements step 5
+    /**
+     * This method randomize the deck
+     * @param deck
+     */
+    public static void shuffleDeck(int[] deck){
+        int[] temp = deck;
+        Random next = new Random();
+        
+        for(int i=0; i<temp.length; i++) {
+            int nextInt = next.nextInt(deck.length);
+            int tempInt = temp[nextInt];
+            temp[nextInt] = temp[i];
+            temp[i] = tempInt;
+        }
+        deck = temp;
+    }
+    
+    /**
+     * This method implements step 3
+     * @param deck
+     */
+    public static void tripleCut(int[] deck){
+        int first = Math.min(findCard(deck, 27), findCard(deck, 28));
+        int second = Math.max(findCard(deck, 27), findCard(deck, 28));
+        
+        int[] aboveFirst = Arrays.copyOfRange(deck, 0, first);
+        int[] belowSecond = Arrays.copyOfRange(deck, second+1, deck.length);
+        int[] unchanged = Arrays.copyOfRange(deck, first, second+1);
+        for(int i=0; i<belowSecond.length; i++) 
+            deck[i] = belowSecond[i];
+        
+        for(int i=0; i<unchanged.length; i++) 
+            deck[i+belowSecond.length] = unchanged[i];
+        
+        for(int i=0; i<aboveFirst.length; i++) 
+            deck[i+belowSecond.length+unchanged.length] = aboveFirst[i];
+        
+    }
+    /**
+     * This method implements step 4
+     * @param deck
+     */
+    public static void sliceFront(int[] deck){
+        int bottom = deck[deck.length-1];
+        if(deck[deck.length-1] == 28)
+            bottom = 27;
+        int[] top = Arrays.copyOfRange(deck, 0, bottom);
+        int[] left = Arrays.copyOfRange(deck, bottom, deck.length);
+        for(int i=0; i<left.length; i++) 
+            deck[i] = left[i];
+        for(int i=0; i<top.length; i++)
+            deck[i+left.length] = top[i];
+    }     
+    
+    /**
+     * This method implements step 5, return 0 if the card is a joker
+     * @param deck
+     * @return
+     */
+    public static int generateKeystreamValue(int[] deck){ 
         int keyStreamValue = 0;
+        int top = deck[0];
+
+        if(deck[0] == 28)
+            top = 27;
+        if(deck[top]==27 || deck[top]==28) 
+            return keyStreamValue;
+        
+        keyStreamValue = deck[top];
         return keyStreamValue;
     }  
-    public static int[] generateKeystream(){
-        int[] keystream = null;
+    /**
+     * This method generates an array of key stream values with the same length as the original text
+     * @param deck
+     * @param length
+     * @return
+     */
+    public static int[] generateKeystream(int[] deck, int length){
+        int[] keystream = new int[length];
+        for(int i=0; i<length; i++) {
+            shuffleDeck(deck);
+            exchangeAJoker(deck);
+            exchangeBJoker(deck);
+            tripleCut(deck);
+            sliceFront(deck);
+            while(generateKeystreamValue(deck) == 0) {
+                shuffleDeck(deck);
+                exchangeAJoker(deck);
+                exchangeBJoker(deck);
+                tripleCut(deck);
+                sliceFront(deck);
+            }
+            keystream[i] = generateKeystreamValue(deck);
+        }
         return keystream;
     }
-    public static String prepareMessage(String message){    // removes all non-alpha characters from "message"
-        String prepared = null;                             // applies uppercase. Research String API
-        return prepared;                                    // pads to factor of 5 characters
+    /**
+     * Removes all non-alpha characters from message, applies uppercase, pads to factors of 5
+     * @param message
+     * @return
+     */
+    public static String prepareMessage(String message){    
+        String prepared = "";
+        int index = 0;
+        char current;
+        while(index<message.length()) {
+            current = message.charAt(index);
+            if(current>=65 && current<=90)
+                prepared += current;
+            if(current>=97 && current<=122)
+                prepared += (char) (current - ('a' - 'A'));
+            if(prepared.length() % 6 == 0)
+                prepared += " ";
+            index++;
+        }  
+        prepared = prepared.substring(1);                          
+        return prepared;                                    
     }
-    public static String encode(String message){            // applies encoding to "message"
+    /**
+     * This method applies encoding to message
+     * @param message
+     * @param key_stream
+     * @return
+     */
+    public static String encode(String message, int[] key_stream){            
         String encoded = "";
+        int[] temp = new int[message.length()];
+        int index = 0;
+        int text_length = message.length();
+        while(index<text_length) {
+            temp[index] = (int) (message.charAt(index) - 'A' + 1);
+            temp[index] = (temp[index] + key_stream[index]) % 26;
+            encoded += (char) (temp[index] + 'A' - 1);
+        }
+        
         return encoded;
     }  
-    public static String decode(String message){            // applies decoding to "message"
+    /**
+     * This method applies decoding to message
+     * @param message
+     * @param key_stream
+     * @return
+     */
+    public static String decode(String message, int[] key_stream){            
         String decoded = "";
+        int[] temp = new int[message.length()];
+        int index = 0;
+        int text_length = message.length();
+        while(index<text_length) {
+            temp[index] = (int) (message.charAt(index) - 'A' + 1);
+            temp[index] = (temp[index] - key_stream[index]) % 26;
+            decoded += (char) (temp[index] + 'A' - 1);
+        }
+
         return decoded;
     } 
 
--- week5/SolitaireTester.java
+++ week5/SolitaireTester.java
@@ -73,6 +73,90 @@ public class SolitaireTester {
         printArray(expected);
     }
 
+    public static void testShuffleDeck() {
+        int[]  cardDeck = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16, // "card deck". Joker A = 27, Joker B = 28
+                            17,18,19,20,21,22,23,24,25,26,27,28}; // no expectaion as the result is random
+        for(int i=0; i<3; i++) {
+            SolitaireCipher.shuffleDeck(cardDeck);
+            printArray(cardDeck);
+            System.out.println("");
+        }
+    }
+
+    public static void testtripleCut() {
+        // Test Cases:
+        // int[] deck = {1,2,28,3,4,5,27,9};
+        // int[] expected = {9,28,3,4,5,27,1,2};
+        // int[] deck = {28,1,2,3,27};
+        // int[] expected = {28,1,2,3,27};
+        int[] deck = {1,2,3,27,4,5,28,9,10};
+        int[] expected = {9,10,27,4,5,28,1,2,3};
+        System.out.println("\nOriginal Deck:");
+        printArray(deck);
+
+        SolitaireCipher.tripleCut(deck);
+
+        if(equalArrays(deck, expected))                         
+            System.out.println("\nexchangeBJoker: TEST PASSED!");
+        else
+            System.out.println("\nexchangeBJoker: TEST FAILED");
+
+        System.out.println("Result:");
+        printArray(deck);
+        System.out.println("\nExpected Result:");
+        printArray(expected);
+    }
+
+    public static void testSliceFront() {
+        // Test Cases:
+        // int[] deck = {2,1};
+        // int[] expected = {1,2};
+        // int[] deck = {4,2,1,3};
+        // int[] expected = {3,4,2,1};
+        int[] deck = {1,2,3,4,6,5};
+        int[] expected = {5,1,2,3,4,6};
+
+        System.out.println("\nOriginal Deck:");
+        printArray(deck);
+
+        SolitaireCipher.sliceFront(deck);
+
+        if(equalArrays(deck, expected))                         
+            System.out.println("\nexchangeBJoker: TEST PASSED!");
+        else
+            System.out.println("\nexchangeBJoker: TEST FAILED");
+
+        System.out.println("Result:");
+        printArray(deck);
+        System.out.println("\nExpected Result:");
+        printArray(expected);
+    }
+
+    public static void testgenerateKeystreamValue() {
+        // Test Cases:
+        // int[] deck = {27,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,28};
+        // int[] expected = 28;
+        // int[] deck = {28,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27};
+        // int[] expected = 27;
+        int[] deck = {2,3,4,5};
+        int expected = 4;
+
+        System.out.println("\nOriginal Deck:");
+        printArray(deck);
+
+        int value = SolitaireCipher.generateKeystreamValue(deck);
+
+        if(value == expected)                         
+            System.out.println("\nexchangeBJoker: TEST PASSED!");
+        else
+            System.out.println("\nexchangeBJoker: TEST FAILED");
+
+        System.out.println("Result:");
+        System.out.println(value);
+        System.out.println("\nExpected Result:");
+        System.out.println(expected);
+    }
+
     // HELPERS. Define any helper methods below here
 
     /**
